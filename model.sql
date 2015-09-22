4f67b10f079f1e1501c52998af1b1c30c7a50088
AlbertPodusenko <albert.podusenko@gmail.com>
2015-09-22 16:10:04 +0300

    Create model.sql

---
+++ model.sql
@@ -0,0 +1,107 @@
+-- tables
+-- Table clients
+CREATE TABLE clients (
+    id int  NOT NULL ,
+    name int  NOT NULL ,
+    contact int  NOT NULL ,
+    CONSTRAINT clients_pk PRIMARY KEY (id)
+);
+
+-- Table materials
+CREATE TABLE materials (
+    id int  NOT NULL ,
+    name int  NOT NULL ,
+    object_id int  NOT NULL ,
+    cost int  NOT NULL ,
+    count int  NOT NULL ,
+    CONSTRAINT materials_pk PRIMARY KEY (id)
+);
+
+-- Table objects
+CREATE TABLE objects (
+    id int  NOT NULL ,
+    name varchar(50)  NOT NULL ,
+    price decimal(12,2)  NOT NULL ,
+    client_id int  NOT NULL ,
+    spending decimal(12,2)  NOT NULL ,
+    CONSTRAINT objects_pk PRIMARY KEY (id)
+);
+
+-- Table objects_relationships
+CREATE TABLE objects_relationships (
+    id int  NOT NULL ,
+    worker_id int  NOT NULL ,
+    prof_id int  NOT NULL ,
+    object_id int  NOT NULL ,
+    CONSTRAINT objects_relationships_pk PRIMARY KEY (id)
+);
+
+-- Table professions
+CREATE TABLE professions (
+    id int  NOT NULL ,
+    name varchar(35)  NOT NULL ,
+    CONSTRAINT professions_pk PRIMARY KEY (id)
+);
+
+-- Table salary
+CREATE TABLE salary (
+    id int  NOT NULL ,
+    object_id int  NOT NULL ,
+    worker_id int  NOT NULL ,
+    type bool  NOT NULL ,
+    salary decimal(8,2)  NOT NULL ,
+    CONSTRAINT salary_pk PRIMARY KEY (id)
+);
+
+-- Table workers
+CREATE TABLE workers (
+    id int  NOT NULL ,
+    name varchar(100)  NOT NULL ,
+    CONSTRAINT workers_pk PRIMARY KEY (id)
+);
+
+
+
+
+
+-- foreign keys
+-- Reference:  materials_objects (table: materials)
+
+
+ALTER TABLE materials ADD CONSTRAINT materials_objects FOREIGN KEY materials_objects (object_id)
+    REFERENCES objects (id);
+-- Reference:  objects_clients (table: objects)
+
+
+ALTER TABLE objects ADD CONSTRAINT objects_clients FOREIGN KEY objects_clients (client_id)
+    REFERENCES clients (id);
+-- Reference:  objects_relationships_objects (table: objects_relationships)
+
+
+ALTER TABLE objects_relationships ADD CONSTRAINT objects_relationships_objects FOREIGN KEY objects_relationships_objects (object_id)
+    REFERENCES objects (id);
+-- Reference:  objects_relationships_professions (table: objects_relationships)
+
+
+ALTER TABLE objects_relationships ADD CONSTRAINT objects_relationships_professions FOREIGN KEY objects_relationships_professions (prof_id)
+    REFERENCES professions (id);
+-- Reference:  objects_relationships_workers (table: objects_relationships)
+
+
+ALTER TABLE objects_relationships ADD CONSTRAINT objects_relationships_workers FOREIGN KEY objects_relationships_workers (worker_id)
+    REFERENCES workers (id);
+-- Reference:  salary_objects (table: salary)
+
+
+ALTER TABLE salary ADD CONSTRAINT salary_objects FOREIGN KEY salary_objects (object_id)
+    REFERENCES objects (id);
+-- Reference:  salary_workers (table: salary)
+
+
+ALTER TABLE salary ADD CONSTRAINT salary_workers FOREIGN KEY salary_workers (worker_id)
+    REFERENCES workers (name);
+
+
+
+-- End of file.
+
